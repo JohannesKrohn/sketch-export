e9e9ebbc078b3181c0a8d56fcd73707376624418
Johannes Krohn <Git@johanneskrohn.de>
2019-07-26 18:26:26 +0200

    Replaced SVGMIN with SVGO added Cheerio to have the possibilty to set colors to all export icons

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,75 +1,115 @@
 var gulp = require('gulp'),
-  sketch = require('gulp-sketch'),
-  svgmin = require('gulp-svgmin'),
-  del = require('del'),
-  imageResize = require('gulp-image-resize'),
-  svgSprite = require("gulp-svg-sprites");
-
+    sketch = require('gulp-sketch'),
+    svgo = require('gulp-svgo'),
+    cheerio = require('gulp-cheerio'),
+    del = require('del'),
+    imageResize = require('gulp-image-resize'),
+    svgSprite = require("gulp-svg-sprites"),
+    $fill = "#EA1C0A";
+config = {
+    mode: {
+        view: { // Activate the «view» mode
+            bust: false,
+            render: {
+                scss: true // Activate Sass output (with default options)
+            }
+        },
+        symbol: true // Activate the «symbol» mode
+    }
+};
 
 gulp.task('svg', function (complete) {
 
-  del('./output/svg/**/**.*')
+    del('./output/**/**.*')
 
-  return gulp.src('./input/*.sketch')
-    .pipe(sketch({
-      export: 'artboards',
-      formats: 'svg'
-    }))
-    .pipe(svgmin({
-        js2svg: {
-          pretty: true
-        }, //js2svg
-        plugins: [
-          {
-            removeAttrs: {
-              attrs: ['stroke.*', 'fill']
-            }
+    return gulp.src('./input/*.sketch')
+        .pipe(sketch({
+            export: 'slices',
+            formats: 'svg'
+        }))
+        .pipe(svgo({
+            js2svg: {
+                pretty: true
+            }, //js2svg
+            plugins: [
 
-          },
-          {removeTitle: true}
-                ]
-      }
+                {
+                    removeAttrs: {
+                        attrs: ['stroke.*', 'fill', 'id', 'class']
+                    }
 
-    )) //svgmin
-    .pipe(gulp.dest('./output/svg/'));
-  complete();
+                }, {
+                    removeTitle: true
+                }, {
+                    collapseGroups: true
+                }, {
+                    removeStyleElement: true
+                }
+            ]
+        })) //svgo
+        .pipe(svgo({
+            js2svg: {
+                pretty: true
+            } //js2svg
+        }))
+        .pipe(gulp.dest('./output/svg/'));
+    complete();
 });
 gulp.task('sprites', function () {
-  return gulp.src('./input/*.sketch')
+    return gulp.src('./input/*.sketch')
         .pipe(sketch({
-          export: 'artboards',
-          formats: 'svg'
+            export: 'slices',
+            formats: 'svg'
         }))
-        .pipe(svgSprite())
+        .pipe(cheerio({
+            parserOptions: { xmlMode: true },
+            run: ($, file, done) => {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+                done();
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('path, rect, circle').attr('fill', $fill);
+                $('svg').append('<rect fill="none" class="icon-area" x="0" y="0" width="48" height="48"/>');
+
+            },
+            parserOptions: {
+                xmlMode: true
+            }
+        }))
+        .pipe(svgSprite()).on('error', function(error){ console.log(error); })
         .pipe(gulp.dest("./output/assets"));
 });
 
 gulp.task('png-iphonex', function (complete) {
-  return gulp.src('./input/*.sketch')
-    .pipe(sketch({
-      export: 'artboards',
-      formats: 'png',
-      scales: 4.0
-    }))
-    .pipe(imageResize({
-      width: 1125,
-      height: 2436,
-      crop: true,
-      gravity: "North",
-      upscale: false
-    }))
-    .pipe(gulp.dest('./output/iphonex'));
+    return gulp.src('./input/*.sketch')
+        .pipe(sketch({
+            export: 'artboards',
+            formats: 'png',
+            scales: 4.0
+        }))
+        .pipe(imageResize({
+            width: 1125,
+            height: 2436,
+            crop: true,
+            gravity: "North",
+            upscale: false
+        }))
+        .pipe(gulp.dest('./output/iphonex'));
 
-  complete();
+    complete();
 });
 gulp.task('png', function (complete) {
-  return gulp.src('./input/*.sketch')
-    .pipe(sketch({
-      export: 'slices',
-      formats: 'png',
-      scales: 2.0
-    }))
-    .pipe(gulp.dest('./output/png'));
+    return gulp.src('./input/*.sketch')
+        .pipe(sketch({
+            export: 'slices',
+            formats: 'png',
+            scales: 2.0
+        }))
+        .pipe(gulp.dest('./output/png'));
 
-  complete();
+    complete();
 });
